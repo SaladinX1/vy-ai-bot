3472ec300d8ac427040c4b51cc38d96ba37f86e9
SaladinX1 <hopefulhorizon01@gmail.com>
2025-07-14 00:06:44 +0200

    fichier  du  dossier config

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GUMROAD_TOKEN = os.getenv("GUMROAD_TOKEN")
+TWITTER_BEARER = os.getenv("TWITTER_BEARER")
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
